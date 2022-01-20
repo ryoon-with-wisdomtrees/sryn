53b24734efd9c4073ce71e751e696bec141e76f0
SphereSRYN <sphere.sryn@gmail.com>
2022-01-20 12:30:08 +0900

    파비콘 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
         lang: `en`,
         display: `standalone`,
         start_url: `/`,
-        icon: `static/favicon.png`,
+        icon: `static/2022.png`,
       },
     },
     {
